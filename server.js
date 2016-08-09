d3f81a99da762c23353ebdbc6346ed7b061632b2
Torgeir Haugen <torgeirhaugen@gmail.com>
2016-08-09 05:43:12 +0000

    working program

--- server.js
+++ server.js
@@ -2,9 +2,47 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(err, res) {
-res.send("Hello world!");
+  app.route("/").get(function(req, res) {
+      //serve index.html, for now just send str
+    //res.sendFile(process.cwd() + '/public/index.html');
+    res.send("<h1>Welcome to my timestamp microservice</h1>");
+  });
+
+app.get('/:str', function(req, res) {
+    var result = decodeString(req.params.str);
+
+    res.send(result);
 });
+
+var decodeString = function(str) {
+    var trimmed = decodeURI(str);
+    var unix_date;
+    var natural_date;
+    
+    if(Number(trimmed)) {
+        //it is a number
+        var number = parseInt(trimmed)*1000;
+    }
+    
+    var date = new Date(number);
+    
+    if(isNaN(date)) {
+        //not a unix number, trying to parse date
+        date = new Date(trimmed);
+    }
+    if(!isNaN(date)) {
+        natural_date = date.toDateString();
+        unix_date = Date.parse(natural_date);
+        
+    } else {
+        unix_date = null;
+        natural_date = null;
+    }
+    return {
+        "unix": unix_date, 
+        "natural": natural_date
+    } 
+}
 
 app.listen('8080', function() {
     console.log("listening...");
